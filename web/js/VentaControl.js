7ec80e83927f5df50c1fea5a7e25e0e9bb792d96
Ricardo Carmona <Ricardo Carmona@DESKTOP-JN0CIEN>
2019-06-23 19:09:46 -0500

    Terminado Javascript de Ventas...

--- web/js/VentaControl.js
+++ web/js/VentaControl.js
@@ -1,24 +1,29 @@
-$(document).ready(function(){
-    
-    $(".addLibroVenta").click(function(){
+$(document).ready(function () {
+
+    $(".addLibroVenta").click(function () {
         var libroVenta = new Array();
-        var i = 0;     
-        $(this).parents("tr").find("td").each(function(){
-            libroVenta[i] = $(this).html();
-            i++;
+        var i = 0;
+        $(this).parents("tr").find("td").each(function () {
+            if (i == 1) {
+                    i++;
+            } else {
+                libroVenta[i] = $(this).html();
+                console.log("Entro al bucle : " + libroVenta[i]);
+                i++;
+            }
+
         });
-       
-        var datos = "<tr><td style='text-align: center;'>"+ libroVenta[0]+ "</td>"
-                    + "<td style='text-align: center;'>" + libroVenta[1] + "</td>"
-                    + "<td style='text-align: center;'>" + libroVenta[2] + "</td>"
-                    + "<td style='text-align: center;'>" + libroVenta[3] + "</td>"
-                    + "<td style='text-align: center;'>" + libroVenta[4] + "</td>"
-                    + "<td style='text-align: center;'>" + libroVenta[5] + "</td>"
-                    + "<td style='text-align: center;'>" + (libroVenta[5]*libroVenta[4]) + "</td>"
-                    + "<td style='text-align: center;'><button style='background-color: #30A048;color: #ffffff;' type='button' class='btn btn-success btn-sm  eliminalinea'><span class='fa fa-close'></span></button> </td>" + "</tr>";
-   
+
+        var datos = "<tr><td style='text-align: center;'>" + libroVenta[0] + "</td>"
+                + "<td style='text-align: center;'>" + libroVenta[1] + "</td>"
+                + "<td style='text-align: center;'>" + libroVenta[2] + "</td>"
+                + "<td style='text-align: center;'>" + libroVenta[3] + "</td>"
+                + "<td style='text-align: center;'>" + libroVenta[4] + "</td>"
+                + "<td style='text-align: center;'>" + libroVenta[5] + "</td>"
+                + "<td style='text-align: center;'>" + (libroVenta[5] * libroVenta[4]) + "</td>"
+                + "<td style='text-align: center;'><button style='background-color: #30A048;color: #ffffff;' type='button' class='btn btn-success btn-sm  eliminalinea'><span class='fa fa-close'></span></button> </td>" + "</tr>";
+
         $("#CuerpoTabla-Venta").append(datos);
-         alert('Codigo del libro : '+libroVenta[0]);
         $.toast({
             heading: 'Libro Seleccionado',
             text: 'Se ha seleccionado correctamente el cliente',
@@ -27,16 +32,15 @@ $(document).ready(function(){
             loaderBg: '#fffff',
             icon: 'success',
             hideAfter: 3500,
-             textColor: 'white',
+            textColor: 'white',
             stack: 6
         });
-        
     });
-    
-    
-    
+
+
+
 });
 
-function CargarIdFactura(){
-    
+function CargarIdFactura() {
+
 }
